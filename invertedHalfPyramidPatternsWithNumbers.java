33924f589c89fa21ec997dc87e3664de8605bedf
Raian-Siddique <raian0197@gmail.com>
2022-11-10 22:05:51 +0100

    final

---
+++ invertedHalfPyramidPatternsWithNumbers.java
@@ -0,0 +1,23 @@
+/**
+ * invertedHalfPyramidPatternsWithNumbers
+ */
+public class invertedHalfPyramidPatternsWithNumbers {
+
+    public static void inverted_Half_Pyramid_WithNumbers(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            // inner numbers
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        inverted_Half_Pyramid_WithNumbers(5);
+    }
+}
